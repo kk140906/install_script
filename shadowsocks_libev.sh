e0c441856ff6f57ac4a7f610aecf04032452cfff
kykx <kkisscj@gmail.com>
2021-11-09 12:26:16 +0800

    add python version

--- shadowsocks_libev.sh
+++ shadowsocks_libev.sh
@@ -2,18 +2,21 @@
 ###
  # @Author       : KK
  # @Date         : 2021-10-21 15:02:24
- # @LastEditTime : 2021-10-22 09:45:13
+ # @LastEditTime : 2021-11-08 10:50:08
  # @LastEditors  : KK
  # @Description  : Just for vps.
  # @FilePath     : \debian_install_script\shadowsocks_libev.sh
 ### 
 
 apt update
-apt install shadowsocks-libev ufw -y
+read -p "\nSelect shadowsocks version:\n1):shaowsocks-libev\n2):shadowsocks-python" version
 read -p "\nInput your server port: " server_port
 read -p "\nInput your password: " password
 
-cat > /etc/shadowsocks/config.json<<EOF
+case ${version} in 
+1)
+    apt install shadowsocks-libev ufw -y
+    cat > /etc/shadowsocks/config.json << EOF
 {
     "server":"0.0.0.0",
     "mode":"tcp_and_udp",
@@ -25,8 +28,33 @@ cat > /etc/shadowsocks/config.json<<EOF
     "method":"aes-256-gcm"
 }
 EOF
+    systemctl start shadowsocks-libev.service
+2)
+    apt install python3-pip libsodium* ufw -y
+    pip install https://github.com/shadowsocks/shadowsocks/archive/master.zip -U
+    cat > /etc/shadowsocks.json << EOF
+{
+    "server":"0.0.0.0",
+    "mode":"tcp_and_udp",
+    "server_port":${server_port},
+    "local_address":"127.0.0.1",
+    "local_port":1854,
+    "password":"${password}",
+    "timeout":60,
+    "method":"aes-256-gcm"
+}
+EOF
+    ssserver -c /etc/shadowsocks.json -d start
+*)
+    echo "invalid input"
+    exit 1
+esac
 
 ufw allow ${server_port}
-systemctl restart shadowsocks-libev.service
+
+echo "install complete"
+echo "server port : " ${server_port}
+echo "password : " ${password}
+echo "method : aes-256-gcm" 
 
 
